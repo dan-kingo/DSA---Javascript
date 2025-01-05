7b99d5d89fc6852c5481981d05abb8d10d27588e
Dan Kingo <danieldejen23@gmail.com>
2025-01-05 08:02:10 +0300

    method one of implementing sum zero

---
+++ sumZero.js
@@ -0,0 +1,25 @@
+/*
+Problem : Write a function called sumZero which accepts a sorted array of integers. The function should find the first pair where the sum is 0. Return an array that includes both values that sum to zero or undefined if a pair does not exist
+
+Test cases:
+sumZero([-3,-2,-1,0,1,2,3]) // [-3,3] 
+sumZero([-2,0,1,3]) // undefined
+sumZero([1,2,3]) // undefined
+*/
+
+// method 1 : using nested loop Big O -> O(n^2)
+const sumZero = (arr) => {
+  // sort the array incase if not
+  arr.sort((a, b) => a - b);
+
+  // iterate over the array
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === 0) {
+        return [arr[i], arr[j]];
+      }
+    }
+  }
+};
+
+console.log(sumZero([5, 2, 8, -3, -4, -2, 0, 3]));
