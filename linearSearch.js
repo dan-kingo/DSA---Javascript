013296dff864ffad61335a967af33df5269c0966
Dan Kingo <danieldejen23@gmail.com>
2025-01-08 12:22:58 +0300

    implement linear search

---
+++ linearSearch.js
@@ -0,0 +1,14 @@
+/* Problem : write a function that does linear search on an array and returns the index of the element if it is found
+
+Test Cases :
+search([1,2,3,4,5,6],4) // 3
+search([1,2,3,4,5,6],6) // 5
+search([1,2,3,4,5,6],11) // -1
+ */
+
+const search = (arr, val) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === val) return i;
+  }
+  return -1;
+};
