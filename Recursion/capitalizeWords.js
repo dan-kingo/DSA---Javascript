dec3a0cbf57138cbbb0f6f7c48e6861c4bc2b86d
Dan Kingo <danieldejen23@gmail.com>
2025-01-13 00:10:11 +0300

    method two of implementing capitalize words

--- Recursion/capitalizeWords.js
+++ Recursion/capitalizeWords.js
@@ -16,3 +16,10 @@ const capitalizeWords = (arr) => {
   }
   return uppercaseArr;
 };
+
+// Method 2: Recursion
+const capitalizeWord = (arr) => {
+  if (arr.length === 0) return [];
+
+  return [arr[0].toUpperCase()].concat(capitalizeWord(arr.slice(1)));
+};
